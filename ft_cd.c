f25d89a114e6221d4d9a27c9b208f1d1e2e47e99
Hoyong Lee <hoylee@student.42seoul.kr>
2021-06-16 19:34:01 +0900

    ft_cd.c

--- ft_cd.c
+++ ft_cd.c
@@ -6,35 +6,40 @@
 /*   By: hoylee <hoylee@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/16 16:37:25 by hoylee            #+#    #+#             */
-/*   Updated: 2021/06/16 17:17:15 by hoylee           ###   ########.fr       */
+/*   Updated: 2021/06/16 18:36:20 by hoylee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	ft_cd(char *string)
+int		ft_cd(char *string)
 {
 	int i;
+	char *pst_buffer;	
 	//char **str_arr;
 	//char *str_pwd;
 
 	i = 0;
-	//str_arr = ft_split(string, '/');
-	if (chdir(string) == -1)
+	if (string == 0 || string[0] == 0)
 	{
-		printf("Error");
+		chdir(getenv("HOME"));
+		return (0);
 	}
-	return ;
-//	while(str_arr[i])
-//	{
-//		pstrBuffer = getcwd( strBuffer, _MAX_PATH );
-//
-//		else if (chdir(string)) == -1)
-//			return (0);
-//		printf("%d = %s\n", i, str_arr[i]);
-//		i++;
-//	}
-
+	pst_buffer = getcwd(0, 0);
+	if (string[0] == '~')
+	{
+		string = string + 1;
+		if(string[0] == '/')
+			string = string + 1;
+		chdir(getenv("HOME"));
+	}
+	if (string[0] != 0 && chdir(string) == -1)
+	{
+		chdir(pst_buffer);
+		printf("Error\n");
+		return (-1);
+	}
+	return (0);
 }
 
 //int main(void)
