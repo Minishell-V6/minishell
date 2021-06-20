6de108190a40aeea72ca8e56c4ebe3dd21834701
hey-chocopie <ghdyd6372@naver.com>
2021-06-20 11:53:41 +0900

    err_print && easy_test Modify

--- error_management.c
+++ error_management.c
@@ -12,17 +12,38 @@
 
 #include "minishell.h"
 
+void			error_write(char *error_str, char *err_cmdline, char *err_cmdline2)
+{
+	int i;
+	int j;
+
+	i = -1;
+	j = -1;
+	if(err_cmdline == 0 || error_str == 0)
+		return ;
+	while(error_str[++i])
+	{
+		j = -1;
+		if(error_str[i] == '%' && error_str[i + 1] == 's' && err_cmdline != 0)
+		{
+			while(err_cmdline[++j])
+				write(2, &err_cmdline[j], 1);
+			err_cmdline = err_cmdline2;
+			i++;
+		}
+		else
+			write(2, &error_str[i], 1);
+	}
+}
+
 void			print_errstr(t_cmd *cmd_list)
 {
 	if (cmd_list->err_manage->errcode == 1)
-		printf("minishell: %s: command not found\n", cmd_list->cmdline[0]);
+		error_write("minishell: %s: command not found\n" ,cmd_list->cmdline[0], 0);
 	else if (cmd_list->err_manage->errcode == 2)
-		printf("minishell: %s: too many arguments\n", cmd_list->cmdline[0]);
+		error_write("minishell: %s: too many arguments\n", cmd_list->cmdline[0], 0);
 	else if (cmd_list->err_manage->errcode == 3)
-		printf("minishell: %s: No such file or directory\n",
-				cmd_list->cmdline[cmd_list->err_manage->errindex]);
+		error_write("minishell: %s: No such file or directory\n", cmd_list->cmdline[cmd_list->err_manage->errindex], 0);
 	else if (cmd_list->err_manage->errcode == 4)
-		printf("minishell: %s: %s: numeric argument required\n",
-				cmd_list->cmdline[0],
-				cmd_list->cmdline[cmd_list->err_manage->errindex]);
+		error_write("minishell: %s: %s: numeric argument required\n" ,cmd_list->cmdline[0], cmd_list->cmdline[cmd_list->err_manage->errindex]);
 }
