51cea6ac5abd6207dae6c699dcb6ed4355b4544f
djeon <du0928@naver.com>
2021-06-29 19:37:33 +0900

    merge the redirection and main

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: seojeong <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 19:54:23 by djeon             #+#    #+#             */
-/*   Updated: 2021/06/25 01:19:01 by mac              ###   ########.fr       */
+/*   Updated: 2021/06/29 16:27:51 by djeon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <fcntl.h>
 
 # define STDIN 			0
 # define STDOUT 		1
@@ -36,6 +37,11 @@ typedef struct	s_cmd
 	int				pipe_flag;
 	int				exit_flag;
 	char			quote;
+	char			*(redirect_filename[4]);
+	//index 0 : left redirect char (<, <<);
+	//index 1 : left redirect filename;
+	//index 2 : righ redirect char (>, >>);
+	//index 3 : righ redirect filename;
 	struct s_err	*err_manage;
 	struct s_cmd	*next;
 }				t_cmd;
@@ -65,4 +71,8 @@ int					haveequal(char *line);
 void				add_key_envp(char ***envp, t_cmd *cmd_list, int keyindex);
 //signal
 void				set_signal(void);
+
+//redirect
+void				redirect(t_cmd *cmd_list);
+int					redirect_check(t_cmd *cmd_list);
 #endif
---
+++ srcs/redirect.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hoylee <hoylee@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/06/29 16:36:03 by hoylee            #+#    #+#             */
+/*   Updated: 2021/06/29 16:36:31 by djeon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int				left_redirect(t_cmd *cmd_list)
+{
+	int			fd;
+
+	fd = open("test", O_RDONLY, 0644);
+	//cmd_list->redirect_filename[3]
+	if (fd < 0)
+	{
+		printf("no surch file");
+		return (-1);
+	}
+	dup2(fd, STDIN);
+	close(fd);
+	return (0);
+}
+
+int				left_redirect_double(t_cmd *cmd_list)
+{
+	char		*line;
+
+	//cmd_list->redirect_filename[3]
+	while (ft_strncmp((line = readline("> ")), "test", 5))
+	{
+		ft_putendl_fd(line, 0);
+	}
+//	char *buf;
+//	int value;
+//	while((value = get_next_line(0, &buf)  > 0))
+//		ft_putendl_fd(buf, STDOUT);
+	return (0);
+}
+
+int				right_redirect(t_cmd *cmd_list)
+{
+	int			fd;
+
+	fd = open("test1234", O_WRONLY | O_CREAT | O_TRUNC, 0744);
+	if (fd < 0)
+	{
+		printf("no surch file");
+		return (-1);
+	}
+	//cmd_list->redirect_filename[3]
+	dup2(fd, STDOUT);
+	close(fd);
+	return (0);
+}
+
+int				right_redirect_double(t_cmd *cmd_list)
+{
+	int			fd;
+
+	fd = open("test1234", O_WRONLY | O_CREAT | O_APPEND, 0744);
+	if (fd < 0)
+	{
+		printf("no surch file");
+		return (-1);
+	}
+	dup2(fd, STDOUT);
+	close(fd);
+	return (0);
+}
+
+void	redirect(t_cmd *cmd_list)
+{
+	if (ft_strncmp("<", cmd_list->redirect_filename[0], 2) == 0)
+		left_redirect(cmd_list);
+	else if (ft_strncmp("<<", cmd_list->redirect_filename[0], 3) == 0)
+		left_redirect_double(cmd_list);
+	if (ft_strncmp(">", cmd_list->redirect_filename[2], 3) == 0)
+		right_redirect(cmd_list);
+	else if (ft_strncmp(">>", cmd_list->redirect_filename[2], 3) == 0)
+		right_redirect_double(cmd_list);
+}
