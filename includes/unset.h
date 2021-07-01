66b67db7441e43232a0d6c516b865df98213609f
Sejin Park <sejpark@student.42seoul.kr>
2021-07-01 17:11:31 +0900

    edit ft_unset function

--- includes/unset.h
+++ includes/unset.h
@@ -6,14 +6,15 @@
 /*   By: sejpark <sejpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:27:53 by sejpark           #+#    #+#             */
-/*   Updated: 2021/06/30 17:40:46 by sejpark          ###   ########.fr       */
+/*   Updated: 2021/07/01 12:01:53 by sejpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UNSET_H
 # define UNSET_H
 # include "structs.h"
+# include "../libft/libft.h"
 
-int ft_unset(t_cmd *cmd_list, char ***envp);
+void ft_unset(t_cmd *cmd_list, char **envp);
 
 #endif
--- srcs/unset.c
+++ srcs/unset.c
@@ -6,14 +6,49 @@
 /*   By: sejpark <sejpark@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/30 16:27:21 by sejpark           #+#    #+#             */
-/*   Updated: 2021/06/30 20:58:28 by sejpark          ###   ########.fr       */
+/*   Updated: 2021/07/01 13:28:25 by sejpark          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/unset.h"
 
-//int ft_unset(t_cmd *cmd_list, char ***envp)
-//{
-//
-//	return (0);
-//}
+int ft_find_envkey(char *key, char **envp)
+{
+	int i;
+	size_t key_len;
+
+	i = 0;
+	key_len = ft_strlen(key);
+	while (envp[i])
+	{
+		if (ft_strncmp(key, envp[i], key_len) == 0 && envp[i][key_len] == '=')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+
+void ft_delete_env(int tgt_idx, char **envp)
+{
+	free(envp[tgt_idx]);
+	while (envp[tgt_idx])
+	{
+		envp[tgt_idx] = envp[tgt_idx + 1];
+		tgt_idx++;
+	}
+}
+
+void ft_unset(t_cmd *cmd_list, char **envp)
+{
+	int i;
+	int tgt_idx;
+
+	i = 0;
+	while (cmd_list->cmdline[i].cmd)
+	{
+		tgt_idx = ft_find_envkey(cmd_list->cmdline[i].cmd, envp);
+		if (tgt_idx > -1)
+			ft_delete_env(tgt_idx, envp);
+		i++;
+	}
+}
