e260dfc7917ab552d38f6644c184856e5501421c
Woong <mac@MACui-MacBook-Air.local>
2021-07-05 00:23:06 +0900

    restrict maximum or minimum argument on exit function

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: mac <marvin@42.fr>                         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/07/02 18:05:08 by mac               #+#    #+#             */
-/*   Updated: 2021/06/30 14:13:58 by djeon            ###   ########.fr       */
+/*   Updated: 2021/07/05 00:04:45 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ char				*ft_strrchr(const char *s, int c);
 size_t				ft_strlen(const char *s);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 char				*ft_strnstr(const char *s, const char *need, size_t len);
-long long			ft_atoi(const char *str);
+long long			ft_atoi(const char *str, int *err_flag);
 void				ft_bzero(void *s, size_t n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memset(void *s, int c, size_t n);
